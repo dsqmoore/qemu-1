3a3dc71ad73cfa16c71deb0a30dc4958bdb6a50e
Patrick Jackson <PatrickSJackson@gmail.com>
2011-06-13 04:14:43 -0400

    Added goldfish_events_device.

--- hw/android_arm.c
+++ hw/android_arm.c
@@ -136,7 +136,7 @@ static void android_arm_init_(ram_addr_t ram_size,
         goldfish_battery_init();
 
     goldfish_add_device_no_io(&event0_device);
-    //events_dev_init(event0_device.base, goldfish_pic[event0_device.irq]);
+    events_dev_init(event0_device.base, goldfish_pic[event0_device.irq]);
 
 #ifdef CONFIG_NAND
     goldfish_add_device_no_io(&nand_device);
