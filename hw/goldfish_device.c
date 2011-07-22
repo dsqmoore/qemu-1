bcec2e3b9024ee61feed6f441850744b3783961c
Patrick Jackson <PatrickSJackson@gmail.com>
2011-07-22 15:59:46 -0400

    Using qdev, no errors but missing output.

---
+++ hw/goldfish_device.c
@@ -0,0 +1,364 @@
+/* Copyright (C) 2007-2008 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+#include "hw.h"
+#include "arm-misc.h"
+#include "goldfish_device.h"
+#ifdef TARGET_I386
+#include "kvm.h"
+#endif
+
+#define PDEV_BUS_OP_DONE        (0x00)
+#define PDEV_BUS_OP_REMOVE_DEV  (0x04)
+#define PDEV_BUS_OP_ADD_DEV     (0x08)
+
+#define PDEV_BUS_OP_INIT        (0x00)
+
+#define PDEV_BUS_OP             (0x00)
+#define PDEV_BUS_GET_NAME       (0x04)
+#define PDEV_BUS_NAME_LEN       (0x08)
+#define PDEV_BUS_ID             (0x0c)
+#define PDEV_BUS_IO_BASE        (0x10)
+#define PDEV_BUS_IO_SIZE        (0x14)
+#define PDEV_BUS_IRQ            (0x18)
+#define PDEV_BUS_IRQ_COUNT      (0x1c)
+
+#include "hw/sysbus.h"
+static struct BusInfo goldfish_bus_info = {
+    .name       = "goldfish-bus",
+    .size       = sizeof(GoldfishBus),
+};
+
+/*
+struct bus_state {
+    struct goldfish_device dev;
+    struct goldfish_device *current;
+};
+*/
+
+typedef struct GoldfishDeviceBusDevice {
+    GoldfishDevice dev;
+} GoldfishDeviceBusDevice;
+
+qemu_irq *goldfish_pic;
+static GoldfishDevice *first_device;
+static GoldfishDevice *last_device;
+uint32_t goldfish_free_base;
+uint32_t goldfish_free_irq;
+
+void goldfish_device_set_irq(GoldfishDevice *dev, int irq, int level)
+{
+    if(irq >= dev->irq_count)
+        cpu_abort (cpu_single_env, "goldfish_device_set_irq: Bad irq %d >= %d\n", irq, dev->irq_count);
+    else
+        qemu_set_irq(goldfish_pic[dev->irq + irq], level);
+}
+
+static int goldfish_add_device_no_io(GoldfishDevice *dev)
+{
+    if(dev->base == 0) {
+        dev->base = goldfish_free_base;
+        goldfish_free_base += dev->size;
+    }
+    if(dev->irq == 0 && dev->irq_count > 0) {
+        dev->irq = goldfish_free_irq;
+        goldfish_free_irq += dev->irq_count;
+    }
+    //printf("goldfish_add_device: %s, base %x %x, irq %d %d\n",
+    //       dev->name, dev->base, dev->size, dev->irq, dev->irq_count);
+    dev->next = NULL;
+    if(last_device) {
+        last_device->next = dev;
+    }
+    else {
+        first_device = dev;
+    }
+    last_device = dev;
+    return 0;
+}
+
+static int goldfish_device_add(GoldfishDevice *dev,
+                       CPUReadMemoryFunc **mem_read,
+                       CPUWriteMemoryFunc **mem_write,
+                       void *opaque)
+{
+    int iomemtype;
+    goldfish_add_device_no_io(dev);
+    // TODO: make sure that is the correct endian format
+    iomemtype = cpu_register_io_memory(mem_read, mem_write, opaque, DEVICE_NATIVE_ENDIAN);
+    cpu_register_physical_memory(dev->base, dev->size, iomemtype);
+    return 0;
+}
+
+static uint32_t goldfish_device_bus_read(void *opaque, target_phys_addr_t offset)
+{
+    GoldfishBus *s = (GoldfishBus *)opaque;
+
+    switch (offset) {
+        case PDEV_BUS_OP:
+            if(s->current) {
+                s->current->reported_state = 1;
+                s->current = s->current->next;
+            }
+            else {
+                s->current = first_device;
+            }
+            while(s->current && s->current->reported_state == 1)
+                s->current = s->current->next;
+            if(s->current)
+                return PDEV_BUS_OP_ADD_DEV;
+            else {
+                goldfish_device_set_irq(&s->dev, 0, 0);
+                return PDEV_BUS_OP_DONE;
+            }
+
+        case PDEV_BUS_NAME_LEN:
+            return s->current ? strlen(s->current->name) : 0;
+        case PDEV_BUS_ID:
+            return s->current ? s->current->id : 0;
+        case PDEV_BUS_IO_BASE:
+            return s->current ? s->current->base : 0;
+        case PDEV_BUS_IO_SIZE:
+            return s->current ? s->current->size : 0;
+        case PDEV_BUS_IRQ:
+            return s->current ? s->current->irq : 0;
+        case PDEV_BUS_IRQ_COUNT:
+            return s->current ? s->current->irq_count : 0;
+    default:
+        cpu_abort (cpu_single_env, "goldfish_bus_read: Bad offset %x\n", offset);
+        return 0;
+    }
+}
+
+static void goldfish_device_bus_op_init(GoldfishBus *s)
+{
+    GoldfishDevice *dev = first_device;
+    while(dev) {
+        dev->reported_state = 0;
+        dev = dev->next;
+    }
+    s->current = NULL;
+    goldfish_device_set_irq(&s->dev, 0, first_device != NULL);
+}
+
+static void goldfish_device_bus_write(void *opaque, target_phys_addr_t offset, uint32_t value)
+{
+    GoldfishBus *s = (GoldfishBus *)opaque;
+
+    switch(offset) {
+        case PDEV_BUS_OP:
+            switch(value) {
+                case PDEV_BUS_OP_INIT:
+                    goldfish_device_bus_op_init(s);
+                    break;
+                default:
+                    cpu_abort (cpu_single_env, "goldfish_bus_write: Bad PDEV_BUS_OP value %x\n", value);
+            };
+            break;
+        case PDEV_BUS_GET_NAME:
+            if(s->current) {
+#ifdef TARGET_I386
+                if(kvm_enabled())
+                    cpu_synchronize_state(cpu_single_env, 0);
+#endif
+                cpu_memory_rw_debug(cpu_single_env, value, (void*)s->current->name, strlen(s->current->name), 1);
+            }
+            break;
+        default:
+            cpu_abort (cpu_single_env, "goldfish_bus_write: Bad offset %x\n", offset);
+    }
+}
+
+static CPUReadMemoryFunc *goldfish_device_bus_readfn[] = {
+    goldfish_device_bus_read,
+    goldfish_device_bus_read,
+    goldfish_device_bus_read
+};
+
+static CPUWriteMemoryFunc *goldfish_device_bus_writefn[] = {
+    goldfish_device_bus_write,
+    goldfish_device_bus_write,
+    goldfish_device_bus_write
+};
+
+/*
+static struct bus_state bus_state = {
+    .dev = {
+        .name = "goldfish_device_bus",
+        .id = -1,
+        .base = 0x10001000,
+        .size = 0x1000,
+        .irq = 1,
+        .irq_count = 1,
+    }
+};
+
+void goldfish_device_init(qemu_irq *pic, uint32_t base, uint32_t size, uint32_t irq, uint32_t irq_count)
+{
+    goldfish_pic = pic;
+    goldfish_free_base = base;
+    goldfish_free_irq = irq;
+}
+
+int goldfish_device_bus_init(uint32_t base, uint32_t irq)
+{
+    bus_state.dev.base = base;
+    bus_state.dev.irq = irq;
+
+    return goldfish_device_add(&bus_state.dev, goldfish_bus_readfn, goldfish_bus_writefn, &bus_state);
+}
+*/
+void goldfish_device_init(qemu_irq *pic, uint32_t base, uint32_t irq)
+{
+    goldfish_pic = pic;
+    goldfish_free_base = base;
+    goldfish_free_irq = irq;
+}
+static int goldfish_device_bus_init(GoldfishDevice *dev)
+{
+    //GoldfishDeviceBusDevice *tdev = (GoldfishDeviceBusDevice *)dev;
+
+    return 0;
+}
+
+DeviceState *goldfish_device_bus_create(GoldfishBus *gbus, uint32_t base, uint32_t irq)
+{
+    DeviceState *dev;
+
+    dev = qdev_create(&gbus->bus, "goldfish-device-bus");
+    qdev_prop_set_uint32(dev, "base", base);
+    qdev_prop_set_uint32(dev, "irq", irq);
+    qdev_init_nofail(dev);
+
+    return dev;
+}
+
+static GoldfishDeviceInfo goldfish_device_bus_info = {
+    .init = goldfish_device_bus_init,
+    .readfn = goldfish_device_bus_readfn,
+    .writefn = goldfish_device_bus_writefn,
+    .qdev.name  = "goldfish-device-bus",
+    .qdev.size  = sizeof(GoldfishDeviceBusDevice),
+    .qdev.props = (Property[]) {
+        DEFINE_PROP_UINT32("base", GoldfishDevice, base, 0x10001000),
+        DEFINE_PROP_UINT32("id", GoldfishDevice, id, -1),
+        DEFINE_PROP_UINT32("size", GoldfishDevice, size, 0x1000),
+        DEFINE_PROP_UINT32("irq", GoldfishDevice, irq, 1),
+        DEFINE_PROP_UINT32("irq_count", GoldfishDevice, irq_count, 1),
+        DEFINE_PROP_END_OF_LIST(),
+    },
+};
+
+static void goldfish_device_bus_register(void)
+{
+    goldfish_bus_register_withprop(&goldfish_device_bus_info);
+}
+device_init(goldfish_device_bus_register);
+
+static int goldfish_busdev_init(DeviceState *qdev, DeviceInfo *qinfo)
+{
+    GoldfishDeviceInfo *info = (GoldfishDeviceInfo *)qinfo;
+    GoldfishDevice *dev = (GoldfishDevice *)qdev;
+    goldfish_device_add(dev, info->readfn, info->writefn, dev);
+/*    char *id;
+
+    if (asprintf(&id, "%s@%x", info->dt_name, dev->reg) < 0) {
+        return -1;
+    }
+
+    dev->qdev.id = id;
+*/
+    return info->init(dev);
+}
+
+void goldfish_bus_register_withprop(GoldfishDeviceInfo *info)
+{
+    info->qdev.init = goldfish_busdev_init;
+    info->qdev.bus_info = &goldfish_bus_info;
+
+    assert(info->qdev.size >= sizeof(GoldfishDevice));
+    qdev_register(&info->qdev);
+}
+
+GoldfishBus *goldfish_bus_init(void)
+{
+    GoldfishBus *bus;
+    BusState *qbus;
+    DeviceState *dev;
+    //DeviceInfo *qinfo;
+
+    /* Create bridge device */
+    dev = qdev_create(NULL, "goldfish-bridge");
+    qdev_init_nofail(dev);
+
+    /* Create bus on bridge device */
+
+    qbus = qbus_create(&goldfish_bus_info, dev, "goldfish-bus");
+    bus = DO_UPCAST(GoldfishBus, bus, qbus);
+
+    /* Init callbacks */
+    //goldfish_device_add(, goldfish_bus_readfn, goldfish_bus_writefn, );
+
+    /* hcall-vio */
+    //spapr_register_hypercall(H_VIO_SIGNAL, h_vio_signal);
+
+    /* hcall-tce */
+    //spapr_register_hypercall(H_PUT_TCE, h_put_tce);
+
+    /* hcall-crq */
+    /*spapr_register_hypercall(H_REG_CRQ, h_reg_crq);
+    spapr_register_hypercall(H_FREE_CRQ, h_free_crq);
+    spapr_register_hypercall(H_SEND_CRQ, h_send_crq);
+    spapr_register_hypercall(H_ENABLE_CRQ, h_enable_crq);
+    */
+
+    /* RTAS calls */
+    //spapr_rtas_register("ibm,set-tce-bypass", rtas_set_tce_bypass);
+    //spapr_rtas_register("quiesce", rtas_quiesce);
+
+    // initialize each existing device with the new bus
+    /*
+    for (qinfo = device_info_list; qinfo; qinfo = qinfo->next) {
+        GoldfishDeviceInfo *info = (GoldfishDeviceInfo *)qinfo;
+
+        if (qinfo->bus_info != &goldfish_bus_info) {
+            continue;
+        }
+
+        if (info->hcalls) {
+            info->hcalls(bus);
+        }
+    }
+    */
+
+    return bus;
+}
+
+static int goldfish_bridge_init(SysBusDevice *dev)
+{
+    /* nothing */
+    return 0;
+}
+
+static SysBusDeviceInfo goldfish_bridge_info = {
+    .init = goldfish_bridge_init,
+    .qdev.name  = "goldfish-bridge",
+    .qdev.size  = sizeof(SysBusDevice),
+    .qdev.no_user = 1,
+};
+
+static void goldfish_register_devices(void)
+{
+    sysbus_register_withprop(&goldfish_bridge_info);
+}
+
+device_init(goldfish_register_devices)
+
---
+++ hw/goldfish_interrupt.c
@@ -0,0 +1,220 @@
+/* Copyright (C) 2007-2008 The Android Open Source Project
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+*/
+#include "hw.h"
+#include "arm-misc.h"
+#include "goldfish_device.h"
+#include "irq.h"
+
+enum {
+    INTERRUPT_STATUS        = 0x00, // number of pending interrupts
+    INTERRUPT_NUMBER        = 0x04,
+    INTERRUPT_DISABLE_ALL   = 0x08,
+    INTERRUPT_DISABLE       = 0x0c,
+    INTERRUPT_ENABLE        = 0x10
+};
+/*
+struct goldfish_int_state {
+    struct goldfish_device dev;
+    uint32_t level;
+    uint32_t pending_count;
+    uint32_t irq_enabled;
+    uint32_t fiq_enabled;
+    qemu_irq parent_irq;
+    qemu_irq parent_fiq;
+};
+*/
+typedef struct GoldfishInterruptDevice {
+    GoldfishDevice dev;
+    uint32_t level;
+    uint32_t pending_count;
+    uint32_t irq_enabled;
+    uint32_t fiq_enabled;
+    qemu_irq parent_irq;
+    qemu_irq parent_fiq;
+} GoldfishInterruptDevice;
+
+static void goldfish_int_update(GoldfishInterruptDevice *s)
+{
+    uint32_t flags;
+
+    flags = (s->level & s->irq_enabled);
+    qemu_set_irq(s->parent_irq, flags != 0);
+
+    flags = (s->level & s->fiq_enabled);
+    qemu_set_irq(s->parent_fiq, flags != 0);
+}
+
+static void goldfish_int_set_irq(void *opaque, int irq, int level)
+{
+    GoldfishInterruptDevice *s = (GoldfishInterruptDevice *)opaque;
+    uint32_t mask = (1U << irq);
+
+    if(level) {
+        if(!(s->level & mask)) {
+            if(s->irq_enabled & mask)
+                s->pending_count++;
+            s->level |= mask;
+        }
+    }
+    else {
+        if(s->level & mask) {
+            if(s->irq_enabled & mask)
+                s->pending_count--;
+            s->level &= ~mask;
+        }
+    }
+    goldfish_int_update(s);
+}
+
+static uint32_t goldfish_int_read(void *opaque, target_phys_addr_t offset)
+{
+    GoldfishInterruptDevice *s = (GoldfishInterruptDevice *)opaque;
+
+    switch (offset) {
+    case INTERRUPT_STATUS: /* IRQ_STATUS */
+        return s->pending_count;
+    case INTERRUPT_NUMBER: {
+        int i;
+        uint32_t pending = s->level & s->irq_enabled;
+        for(i = 0; i < 32; i++) {
+            if(pending & (1U << i))
+                return i;
+        }
+        return 0;
+    }
+    default:
+        cpu_abort (cpu_single_env, "goldfish_int_read: Bad offset %x\n", offset);
+        return 0;
+    }
+}
+
+static void goldfish_int_write(void *opaque, target_phys_addr_t offset, uint32_t value)
+{
+    GoldfishInterruptDevice *s = (GoldfishInterruptDevice *)opaque;
+    uint32_t mask = (1U << value);
+
+    switch (offset) {
+        case INTERRUPT_DISABLE_ALL:
+            s->pending_count = 0;
+            s->level = 0;
+            break;
+
+        case INTERRUPT_DISABLE:
+            if(s->irq_enabled & mask) {
+                if(s->level & mask)
+                    s->pending_count--;
+                s->irq_enabled &= ~mask;
+            }
+            break;
+        case INTERRUPT_ENABLE:
+            if(!(s->irq_enabled & mask)) {
+                s->irq_enabled |= mask;
+                if(s->level & mask)
+                    s->pending_count++;
+            }
+            break;
+
+    default:
+        cpu_abort (cpu_single_env, "goldfish_int_write: Bad offset %x\n", offset);
+        return;
+    }
+    goldfish_int_update(s);
+}
+
+static CPUReadMemoryFunc *goldfish_int_readfn[] = {
+    goldfish_int_read,
+    goldfish_int_read,
+    goldfish_int_read
+};
+
+static CPUWriteMemoryFunc *goldfish_int_writefn[] = {
+    goldfish_int_write,
+    goldfish_int_write,
+    goldfish_int_write
+};
+/*
+qemu_irq*  goldfish_interrupt_init(uint32_t base, qemu_irq parent_irq, qemu_irq parent_fiq)
+{
+    int ret;
+    struct goldfish_int_state *s;
+    qemu_irq*  qi;
+
+    s = qemu_mallocz(sizeof(*s));
+    qi = qemu_allocate_irqs(goldfish_int_set_irq, s, 32);
+    s->dev.name = "goldfish_interrupt_controller";
+    s->dev.id = -1;
+    s->dev.base = base;
+    s->dev.size = 0x1000;
+    s->parent_irq = parent_irq;
+    s->parent_fiq = parent_fiq;
+
+    ret = goldfish_device_add(&s->dev, goldfish_int_readfn, goldfish_int_writefn, s);
+    if(ret) {
+        qemu_free(s);
+        return NULL;
+    }
+
+    return qi;
+}
+
+*/
+
+static int goldfish_int_init(GoldfishDevice *dev)
+{
+    GoldfishInterruptDevice *idev = (GoldfishInterruptDevice *)dev;
+    qemu_irq* qi;
+    
+    qi = qemu_allocate_irqs(goldfish_int_set_irq, idev, 32);
+    goldfish_device_init(qi, 0xff010000, 10);
+
+    //goldfish_device_add(&rdev->dev, goldfish_int_readfn, goldfish_int_writefn, dev);
+    return 0;
+}
+
+DeviceState *goldfish_int_create(GoldfishBus *gbus, uint32_t base, qemu_irq parent_irq, qemu_irq parent_fiq)
+{
+    DeviceState *dev;
+    GoldfishDevice *gdev;
+    GoldfishInterruptDevice *idev;
+
+    dev = qdev_create(&gbus->bus, "goldfish-int");
+    qdev_prop_set_uint32(dev, "id", -1);
+    qdev_prop_set_uint32(dev, "base", base);
+    qdev_prop_set_uint32(dev, "size", 0x1000);
+    qdev_init_nofail(dev);
+    gdev = (GoldfishDevice *)dev;
+    idev = DO_UPCAST(GoldfishInterruptDevice, dev, gdev);
+    idev->parent_irq = parent_irq;
+    idev->parent_fiq = parent_fiq;
+
+    return dev;
+}
+
+static GoldfishDeviceInfo goldfish_int_info = {
+    .init = goldfish_int_init,
+    .readfn = goldfish_int_readfn,
+    .writefn = goldfish_int_writefn,
+    .qdev.name  = "goldfish-int",
+    .qdev.size  = sizeof(GoldfishInterruptDevice),
+    .qdev.props = (Property[]) {
+        DEFINE_PROP_UINT32("id", GoldfishDevice, id, -1),
+        DEFINE_PROP_UINT32("base", GoldfishDevice, base, 0),
+        DEFINE_PROP_UINT32("size", GoldfishDevice, size, 0x1000),
+        DEFINE_PROP_END_OF_LIST(),
+    },
+};
+
+static void goldfish_int_register(void)
+{
+    goldfish_bus_register_withprop(&goldfish_int_info);
+}
+device_init(goldfish_int_register);
